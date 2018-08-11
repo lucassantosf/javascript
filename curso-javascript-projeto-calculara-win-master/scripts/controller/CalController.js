994e30129511d149ede21b63887af84698c9cea1
lucassantosf <lucas46em@gmail.com>
2018-08-11 18:59:24 -0300

    Hora e data dinamica, eventos sobre botoes numericos

--- curso-javascript-projeto-calculara-win-master/scripts/controller/CalController.js
+++ curso-javascript-projeto-calculara-win-master/scripts/controller/CalController.js
@@ -3,41 +3,76 @@ class CalcController{
 	//Método Construtor
 	constructor(){
 		// quando declarado com this. em qualquer lugar da classe esta váriavel poderá ser utilizada
-		// Por Convensão atributos com _ são private		
-		this._displayCalc = "0";
+		// Por Convensão atributos com _ são private				
+		this._locale = 'pt-BR'; // Variável para definir o idioma nas funções de tempo e hora
+		this._displayCalcEl = document.querySelector("#display"); // Variáveis selecionam os elementos do body pelo seu ID
+		this._dateEl = document.querySelector("#data");
+		this._timeEl = document.querySelector("#hora");		
 		this._currentDate;
 		this.initialize();
-
+		this.initButtonsEvents();
 	}
 	//Tudo que irá acontecer quando iniciar o objeto
-	initialize(){
+	initialize(){		
 		
-		// Variáveis selecionam os elementos do body pelo seu ID
-		let displayCalcEl = document.querySelector("#display");
-		let dateEl = document.querySelector("#data");
-		let timeEl = document.querySelector("#hora");
+		this.setDisplayDateTime();//Define no HTML a data e hora atual
 
-		displayCalcEl.innerHTML = "435" ;//Com o elemento selecionado, .innerHTML converte um valor dentro do body em HTML
-		dateEl.innerHTML = "435" ;
-		timeEl.innerHTML = "435" ;	
+		//Função executada em intervalo de tempo, neste caso executa uma arrow function em 1000 milisegundos
+		setInterval(()=>{
+			this.setDisplayDateTime();
+		},1000);	
+		
+		/*setTimeout(()=>{ //Esta função aguarda um determinado período para executar
+			clearInterval(interval);
+		},10000);*/
 
+	}
+	//Eventos dos botões
+	initButtonsEvents(){
+		let buttons = document.querySelectorAll("#calculadora > div, #buttons button"); //Seleciona todos os elementos pelo ID e o tipo do elemento
+		
+		buttons.forEach((btn,index)=>{
+			btn.addEventListener('click',e=>{//Apartir do evento do click, precisa-se da classe do elemento clicado
+				console.log(btn.className.replace("btn btn-number col-sm  btn-","")); 
+			});
+		});
+
+		
 	}
 
 	// Encapsulamentos ----- Getters e Setter -----------------------------------------------------------------------------------------------------------------------------------------------
 	// Get's	
 	get displayCalc(){
-		return this._displayCalc;
+		//pega o valor que esta no html
+		return this._displayCalcEl.innerHTML;
 	}
-	get dataAtual(){
-		return this._currentDate;
+	get currentDate(){
+		return new Date();
+	}
+	get displayTime(){//relacionado à hora
+		return this._timeEl.innerHTML;
+	}
+	get displayDate(){//relacionado à data
+		return this._dateEl.innerHTML;
 	}
 
 	// Set's
 	set displayCalc(valor){
-		this._displayCalc = valor;
+		//altera o valor no HTML
+		this._displayCalcEl.innerHTML = valor;
 	}
-	set dataAtual(valor){
+	set currentDate(valor){
 		this._currentDate = valor;
+	}
+	set displayTime(valor){//relacionado à hora
+		this._timeEl.innerHTML = valor;
+	}
+	set displayDate(valor){//relacionado à data
+		this._dateEl.innerHTML = valor;
+	}
+	setDisplayDateTime(){ //Este método exibe a data e hora atual nos elementos HTML
+		this.displayDate = this.currentDate.toLocaleDateString(this._locale,{day:"2-digit",month:"short",year:"numeric"});//seta o display de Data com o valor da Data Atual recebido objeto do método currentDate
+		this.displayTime = this.currentDate.toLocaleTimeString(this._locale);//seta o display de Tempo com o valor do Tempo Atual recebido objeto do método currentDate
 	}
 
 	// Fim Encapsulamento ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
