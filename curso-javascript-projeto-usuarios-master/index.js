446030b7dc6ae46efc33ff8867ca47b34ec2cd9a
lucassantosf <lucas46em@gmail.com>
2018-08-27 19:46:19 -0300

    Retrofit Projeto Usuários

--- curso-javascript-projeto-usuarios-master/index.js
+++ curso-javascript-projeto-usuarios-master/index.js
@@ -1,6 +1,18 @@
-let userController = new UserController("form-user-create", "form-user-update", "table-users");
+var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 
+document.getElementById("form-user-create").addEventListener("submit",function(event){
+	
+	event.preventDefault();
 
-
-
-
+	fields.forEach(function(field, index){
+	
+		if(field.name == "gender" && field.checked){
+			user[field.name] = field.value;
+		}else{
+			user[field.name] = field.value;
+		}
+	
+	});
+	console.log(user);
+});
