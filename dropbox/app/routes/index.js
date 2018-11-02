924146e3edc244a550371facf07b3a9b22cd1b4f
lucassantosf <lucas46em@gmail.com>
2018-11-02 20:17:03 -0300

    Criar pastas, e navegar entre pastas

--- dropbox/app/routes/index.js
+++ dropbox/app/routes/index.js
@@ -7,6 +7,33 @@ var fs = require('fs');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/file', (req,res)=>{
+	
+	let path = './' + req.query.path;
+
+	if(fs.existsSync(path)){
+
+		fs.readFile(path, (err, data)=>{
+
+			if(err){
+
+				console.error(err);
+				res.status(400).json({
+					error:err
+				});
+
+			}else{
+				res.status(200).end(data);
+			}
+		})
+	}else{
+
+		res.status(404).json({
+			error:'File not found'
+		});
+	}
+});
 	
 router.delete('/file', (req,res)=>{
 	
@@ -35,7 +62,12 @@ router.delete('/file', (req,res)=>{
 
 			});
 
-		}
+		}else{
+
+		res.status(404).json({
+			error:'File not found'
+		});
+	}
 		
 	});
 
