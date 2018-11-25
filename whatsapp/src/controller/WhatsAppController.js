d9b09dd8f0dea8a9866c682a5596c3662885522e
lucassantosf <lucas46em@gmail.com>
2018-11-25 17:33:49 -0200

    Eventos em menus laterais e Menu Anexar

--- whatsapp/src/controller/WhatsAppController.js
+++ whatsapp/src/controller/WhatsAppController.js
@@ -2,14 +2,13 @@ class WhatsAppController{
 	
 	constructor(){
 
-		console.log('Whats Controller OK');
+		console.log('WhatsAppController OK');
 
 		this.elementsPrototype();
 
 		this.loadElements();
 
 		this.initEvents();
-
 	}
 
 	loadElements(){
@@ -17,9 +16,7 @@ class WhatsAppController{
 		this.el = {};
 
 		document.querySelectorAll('[id]').forEach(element=>{
-
 			this.el[Format.getCamelCase(element.id)] = element;
-
 		});
 	}
 
@@ -28,24 +25,24 @@ class WhatsAppController{
 		Element.prototype.hide = function(){
 			this.style.display = 'none';
 			return this;
-		};
+		}
 
 		Element.prototype.show = function(){
 			this.style.display = 'block';
 			return this;
-		};
+		}
 
 		Element.prototype.toggle = function(){
 			this.style.display = (this.style.display === 'none') ? 'block' : 'none';
 			return this;
-		};
+		}
 
-		Element.prototype.on = function(event, fn){
-			event.split('').forEach(event=>{
+		Element.prototype.on = function(events, fn){
+			events.split(' ').forEach(event=>{
 				this.addEventListener(event, fn);
-			});
+			});	
 			return this;
-		};	
+		}
 
 		Element.prototype.css = function(styles){
 			for(let name in styles){
@@ -73,7 +70,17 @@ class WhatsAppController{
 			return this.classList.contains(name);
 		}
 
+		HTMLFormElement.prototype.getForm = function(){
+			return new FormData(this);
+		}
 
+		HTMLFormElement.prototype.toJSON = function(){
+			let json = {};
+			this.getForm().forEach((value, key)=>{
+				json[key] = value;
+			});
+			return json;
+		}
 	}
 
 	initEvents(){
@@ -83,9 +90,7 @@ class WhatsAppController{
 			this.el.panelEditProfile.show();
 			setTimeout(()=>{
 				this.el.panelEditProfile.addClass('open');
-			}, 300);
-			
-		
+			},300);				
 		});
 
 		this.el.btnNewContact.on('click', e=>{
@@ -93,27 +98,130 @@ class WhatsAppController{
 			this.el.panelAddContact.show();
 			setTimeout(()=>{
 				this.el.panelAddContact.addClass('open');
-			}, 300);
-			
-
+			},300);			
 		});
 
 		this.el.btnClosePanelEditProfile.on('click', e=>{
-
 			this.el.panelEditProfile.removeClass('open');
 		});
 
 		this.el.btnClosePanelAddContact.on('click', e=>{
-
 			this.el.panelAddContact.removeClass('open');
+		});
+
+		this.el.photoContainerEditProfile.on('click', e=>{
+			this.el.inputProfilePhoto.click();
+		});
+
+		this.el.inputNamePanelEditProfile.on('keypress',e=>{
+			if(e.key === 'Enter'){
+				e.preventDefault();
+				this.el.btnSavePanelEditProfile.click();
+			}
+		});
+
+		this.el.btnSavePanelEditProfile.on('click',e=>{
+			console.log(this.el.inputNamePanelEditProfile.innerHTML);
+		});
+
+		this.el.formPanelAddContact.on('submit', e=>{
+			e.preventDefault();
+			let formData = new FormData(this.el.formPanelAddContact);
+		});
+
+		this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item=>{
+			
+			item.on('click',e=>{
+					
+				this.el.home.hide();
+				this.el.main.css({
+					display:'flex'
+				});
+			});
+
+		});
+
+		this.el.btnAttach.on('click',e=>{
+			
+			e.stopPropagation();
+			this.el.menuAttach.addClass('open');
+
+			document.addEventListener('click',this.closeMenuAttach.bind(this));
+
+		});
+
+		this.el.btnAttachPhoto.on('click', e=>{
+
+			this.el.inputPhoto.click();
+		});
+
+		this.el.inputPhoto.on('change',e=>{
+			console.log(this.el.inputPhoto.files);
+
+			[...this.el.inputPhoto.files].forEach(file=>{
+				console.log(file);
+			});
+		});
+
+		this.el.btnAttachCamera.on('click', e=>{
+			this.closeAllMainPanel();
+			this.el.panelCamera.addClass('open');
+			this.el.panelCamera.css({
+				'height':'calc(100% - 120px)'
+			});
+		});
+
+		this.el.btnTakePicture.on('click',e=>{
+			console.log("Tirar foto");
+		});
+
+		this.el.btnClosePanelCamera.on('click',e=>{
+			this.closeAllMainPanel();
+			this.el.panelMessagesContainer.show();
 		});	
+
+		this.el.btnAttachDocument.on('click', e=>{
+			
+			this.closeAllMainPanel();
+			this.el.panelDocumentPreview.addClass('open');
+			this.el.panelDocumentPreview.css({
+				'height':'calc(100% - 120px)'
+			});
+		});
+
+		this.el.btnClosePanelDocumentPreview.on('click',e=>{
+			this.closeAllMainPanel();
+			this.el.panelMessagesContainer.show();
+		});
+
+		this.el.btnSendDocument.on('click',e=>{
+			console.log('Send Document');
+		});
+
+		this.el.btnAttachContact.on('click', e=>{
+			this.el.modalContacts.show();	
+		});
+
+		this.el.btnCloseModalContacts.on('click',e=>{
+			this.el.modalContacts.hide();
+		});
+	}
+
+	closeAllMainPanel(){
+
+		this.el.panelMessagesContainer.hide();
+		this.el.panelDocumentPreview.removeClass('open');
+		this.el.panelCamera.removeClass('open');
+	}
+
+	closeMenuAttach(e){
+		document.removeEventListener('click', this.closeMenuAttach);
+		this.el.menuAttach.removeClass('open');
+
 	}
 
 	closeAllLeftPanel(){
-
-		this.el.panelEditProfile.hide();
 		this.el.panelAddContact.hide();
-
+		this.el.panelEditProfile.hide();
 	}
-
 }
