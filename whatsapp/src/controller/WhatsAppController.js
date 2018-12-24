4c0590b8158e65fbfb719b708f33cfe02289701c
lucassantosf <lucas46em@gmail.com>
2018-12-24 19:12:27 -0200

    Utilizar WebCan com Camera WebApi device

--- whatsapp/src/controller/WhatsAppController.js
+++ whatsapp/src/controller/WhatsAppController.js
@@ -169,6 +169,10 @@ class WhatsAppController{
 			this.el.panelCamera.css({
 				'height':'calc(100% - 120px)'
 			});
+
+			this._camera = new CameraController(this.el.videoCamera);
+
+
 		});
 
 		this.el.btnTakePicture.on('click',e=>{
