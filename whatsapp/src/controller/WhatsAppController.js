55f4757358fb662bc669d51405db3141341950b6
lucassantosf <lucas46em@gmail.com>
2019-04-01 19:28:10 -0300

    Filtrar Contatos e atualizando status de msg

--- whatsapp/src/controller/WhatsAppController.js
+++ whatsapp/src/controller/WhatsAppController.js
@@ -169,12 +169,25 @@ export class WhatsAppController{
 				
 				data.id = doc.id;
 				
+ 				let message = new Message();
+
+				message.fromJSON(data);
+				
+				let me = (data.from === this._user.email);
+				
 				if(!this.el.panelMessagesContainer.querySelector('#_'+data.id)){					
- 					let message = new Message();
-					message.fromJSON(data);
-					let me = (data.from === this._user.email);
+					if(!me){
+						doc.ref.set({
+							status: 'read'
+						},{
+							merge: true
+						});
+					}
 					let view = message.getViewElement(me);
 					this.el.panelMessagesContainer.appendChild(view);					
+				}else if(me){
+					let msgEl = this.el.panelMessagesContainer.querySelector('#_'+data.id);
+					msgEl.querySelector('.message-status').innerHTML = message.getStatusViewElement().outerHTML;
 				}				
 			});
 
@@ -259,6 +272,15 @@ export class WhatsAppController{
 	}
 
 	initEvents(){
+
+		this.el.inputSearchContacts.on('keyup',e=>{
+			if(this.el.inputSearchContacts.value.length > 0 ){
+				this.el.inputSearchContactsPlaceholder.hide();
+			}else{
+				this.el.inputSearchContactsPlaceholder.show();
+			}
+			this._user.getContacts(this.el.inputSearchContacts.value);
+		});
 
 		this.el.myPhoto.on('click', e=>{
 			this.closeAllLeftPanel();
