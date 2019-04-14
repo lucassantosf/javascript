e591d44d1c400700699cc119e2b59599f6b074b6
lucassantosf <lucas46em@gmail.com>
2019-04-14 19:59:39 -0300

    Enviar documentos como anexo

--- whatsapp/src/model/Message.js
+++ whatsapp/src/model/Message.js
@@ -23,6 +23,24 @@ export class Message extends Model{
 	get status(){ return this._data.status; }
 	set status(value){ this._data.status = value;}
 
+	get preview(){ return this._data.preview; }
+	set preview(value){ this._data.preview = value;}
+
+	get info(){ return this._data.info; }
+	set info(value){ this._data.info = value;}
+
+	get fileType(){ return this._data.fileType; }
+	set fileType(value){ this._data.fileType = value;}
+
+	get filename(){ return this._data.filename; }
+	set filename(value){ this._data.filename = value;}
+
+	get size(){ return this._data.size; }
+	set size(value){ this._data.size = value;}
+
+	get from(){ return this._data.from; }
+	set from(value){ this._data.from = value;}
+
 	getViewElement(me = true){
 		let div = document.createElement('div');
 		div.className = 'message';
@@ -96,13 +114,13 @@ export class Message extends Model{
                     <div class="_3_7SH _1ZPgd " id="_${this.id}">
 	                    <div class="_1fnMt _2CORf">
 	                        <a class="_1vKRe" href="#">
-	                            <div class="_2jTyA" style="background-image: url()"></div>
+	                            <div class="_2jTyA" style="background-image: url(${this.preview})"></div>
 	                            <div class="_12xX7">
 	                                <div class="_3eW69">
 	                                    <div class="JdzFp message-file-icon icon-doc-pdf"></div>
 	                                </div>
 	                                <div class="nxILt">
-	                                    <span dir="auto" class="message-filename">Arquivo.pdf</span>
+	                                    <span dir="auto" class="message-filename">${this.filename}</span>
 	                                </div>
 	                                <div class="_17viz">
 	                                    <span data-icon="audio-download" class="message-file-download">
@@ -120,9 +138,9 @@ export class Message extends Model{
 	                            </div>
 	                        </a>
 	                        <div class="_3cMIj">
-	                            <span class="PyPig message-file-info">32 páginas</span>
-	                            <span class="PyPig message-file-type">PDF</span>
-	                            <span class="PyPig message-file-size">4 MB</span>
+	                            <span class="PyPig message-file-info">${this.info}</span>
+	                            <span class="PyPig message-file-type">${this.fileType}</span>
+	                            <span class="PyPig message-file-size">${this.size}</span>
 	                        </div>
 	                        <div class="_3Lj_s">
 	                            <div class="_1DZAH" role="button">
@@ -133,6 +151,9 @@ export class Message extends Model{
 	                	</div>
                 	</div>           
 	 			`;
+	 			div.on('click',e=>{
+	 				window.open(this.content);
+	 			});
 	 		break;
 	 		case 'audio':
 	 			div.innerHTML = `
@@ -254,12 +275,15 @@ export class Message extends Model{
 				type,
 				from 
 			}).then(result=>{
-				result.parent.doc(result.id).set({
+				
+				let docRef = result.parent.doc(result.id);
+
+				docRef.set({
 					status:'sent'
 				},{
 					merge:true
 				}).then(()=>{
-					s();
+					s(docRef);
 				});
 			});
 		});
@@ -270,35 +294,92 @@ export class Message extends Model{
 			.collection('chats')
 			.doc(chatId)
 			.collection('messages');
-	}
+	}	
 
-	static sendImage(chatId, from, file){ 
-        return new Promise((s, f) => {
- 
-            let uploadTask = Firebase.hd().ref(from).child(Date.now() + '_' + file.name).put(file);
+	static upload(file, from){
+
+		return new Promise((s,f)=>{
+			let uploadTask = Firebase.hd().ref(from).child(Date.now() + '_' + file.name).put(file);
  
             uploadTask.on('state_changed', e => { 
                 console.info('upload', e); 
             }, err => {
-                console.error(err)
+                f(err);
             }, () => { 
                 uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
-                    Message.send(
-                        chatId, 
-                        from, 
-                        'image', 
-                        downloadURL                    
-                    ).then(() => {
-                        s();
-                    });
+                    s(downloadURL);
                 });
          
             });
- 
-        });
- 
-    }
+		
+		});
+		
+	}
 
+	static sendDocument(chatId, from, file, filePreview, info){
+
+		Message.send(chatId, from, 'document','').then(msgRef =>{
+
+		 	Message.upload(file, from).then(downloadURL=>{
+	    
+	    		let downloadFile = downloadURL;
+
+	    		if(filePreview){
+	    			Message.upload(filePreview, from).then(downloadURL=>{
+	    
+		    		let downloadPreview = downloadURL;
+
+		    		msgRef.set({
+		    			content: downloadFile,
+		    			priview: downloadPreview,
+		    			filename: file.name,
+		    			size: file.size,
+		    			filetype: file.type,
+		    			status: 'sent' ,
+		    			info
+		    		}, {
+		    			merge:true
+		    			});
+		    		});
+	    		}else{
+	    			
+	    			msgRef.set({
+		    			content: downloadFile,
+		    			priview: downloadPreview,
+		    			filename: file.name,
+		    			size: file.size,
+		    			filetype: file.type,
+		    			status: 'sent' 
+		    		}, {
+		    			merge:true
+		    		}); 
+
+	    		}
+	    	
+	    	});
+
+		});		
+	}
+
+	static sendImage(chatId, from, file){ 
+        
+        return new Promise((s, f) => {
+ 			
+        	Message.upload(file, from).then(downloadURL=>{
+        
+        		Message.send(
+                    chatId, 
+                    from, 
+                    'image', 
+                    downloadURL                    
+                ).then(() => {
+                    s();
+                });
+
+        	}); 
+
+        });
+    }
 
 	getStatusViewElement(){
 		let div = document.createElement('div');
