5b418677cef8d62ffd05b5f8276cdfac571fb822
lucassantosf <lucas46em@gmail.com>
2019-05-01 16:31:49 -0300

    Exibir dados do banco com EJS

--- restaurante/saboroso/routes/index.js
+++ restaurante/saboroso/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var conn = require('./../inc/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	conn.query(`
+		select * from tb_menus order by title
+	`,(err,results)=>{
+		if(err){
+			console.log(err);
+		}
+
+  		res.render('index', {  
+	  		title: 'Restaurante Saboroso',
+	  		menus: results
+  		});
+
+	});
 });
 
 module.exports = router;
