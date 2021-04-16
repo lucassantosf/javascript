083611a7eec11328394fbd824737310897570f4c
unknown <lucas46em@gmail.com>
2021-04-16 17:18:48 -0300

    sequelize connection in postgres, testing it on mocha

--- nodejs/multidb/src/PostgresExample.js
+++ nodejs/multidb/src/PostgresExample.js
@@ -1,1 +1,47 @@
 // npm install sequelize
+const Sequelize = require('sequelize')
+
+const driver = new Sequelize(
+    'heroes',
+    'lucas',
+    '123456',{
+        host: 'localhost',
+        dialect: 'postgres',
+        quoteIdentifiers: false,
+        operatorsAliases: false
+    }
+)
+
+async function main(){
+    const Herois = driver.define('heroes',{
+        id:{
+            type: Sequelize.INTEGER,
+            require: true,
+            primaryKey:true,
+            autoIncrement: true
+        },
+        nome:{
+            type: Sequelize.STRING,
+            require: true, 
+        },
+        poder:{
+            type: Sequelize.STRING,
+            require: true, 
+        }
+    },{
+        tableName: 'tb_herois',
+        freezeTableName: false,
+        timestamps: false
+    })
+    await Herois.sync()
+
+    await Herois.create({
+        nome: 'Lanterna Verde',
+        poder: 'Anel'
+    })
+
+    const result = await Herois.findAll({ raw: true, attributes:['nome']})
+    console.log('result',result)
+}
+
+main()
--- nodejs/multidb/src/db/strategies/postgres.js
+++ nodejs/multidb/src/db/strategies/postgres.js
@@ -1,11 +1,56 @@
 const ICrud = require('./interfaces/interfaceCrud')
-
+const Sequelize = require('sequelize')
+ 
 class Postgres extends ICrud{
     constructor(){
         super()
+        this._driver = null
+        this._herois = null
+        this._connect()
     }
-    isConnected(){
-        
+    async isConnected(){
+        try {
+            await this._driver.authenticate()
+            return true
+        } catch (error) {
+            console.log('fail',error)
+            return false
+        }
+    }
+    async defineModel(){
+        this._herois = driver.define('heroes',{
+            id:{
+                type: Sequelize.INTEGER,
+                require: true,
+                primaryKey:true,
+                autoIncrement: true
+            },
+            nome:{
+                type: Sequelize.STRING,
+                require: true, 
+            },
+            poder:{
+                type: Sequelize.STRING,
+                require: true, 
+            }
+        },{
+            tableName: 'tb_herois',
+            freezeTableName: false,
+            timestamps: false
+        })
+        await Herois.sync()
+    }
+    _connect(){
+        this._driver = new Sequelize(
+            'heroes',
+            'lucas',
+            '123456',{
+                host: 'localhost',
+                dialect: 'postgres',
+                quoteIdentifiers: false,
+                operatorsAliases: false
+            }
+        )
     }
     create(item){
         console.log('salvo postgres')
