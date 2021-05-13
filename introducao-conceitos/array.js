c7cf518581a08d4dab40b2e8ac623f8f50aa2d54
unknown <lucas46em@gmail.com>
2021-05-13 17:13:39 -0300

    not used files

--- introducao-conceitos/array.js
+++
@@ -1,8 +0,0 @@
-let carros = ["palio 98", "toro", "uno", 12, true, new Date()]; // array em JavaScript aceita qualquer tipo de dados
-
-console.log(carros.length);
-console.log(carros[0]);
-
-carros.forEach(function(value, index){
-	console.log(index, value);
-});
