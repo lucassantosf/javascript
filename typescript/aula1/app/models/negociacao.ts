d276a67b3d091e876f507a1cd387a5471c8feed6
Lucas <lucas46em@gmail.com>
2022-02-16 12:29:54 -0300

    init with typescript

---
+++ typescript/aula1/app/models/negociacao.ts
@@ -0,0 +1,17 @@
+export class Negociacao{
+    private _data;
+    private _quantidade;
+    private _valor;
+
+    constructor(data,quantidade,valor)
+    {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    get data()
+    {
+        return this.data
+    }
+}
