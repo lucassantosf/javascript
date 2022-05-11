4348b1aea603642c89cf0e881d21f9707e1a0d2b
Lucas <lucas46em@gmail.com>
2022-05-11 17:12:02 -0300

    include entity and module injection

--- nestjs/aula2/src/usuario/usuario.controller.ts
+++ nestjs/aula2/src/usuario/usuario.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, Param } from "@nestjs/common";
+import { Usuario } from "./usuario.entity";
 import { UsuarioService } from "./usuario.service";
 
 @Controller('users')
 export class UsuarioController{
     
-    private usuarioService = new UsuarioService()
+    constructor(private usuarioService: UsuarioService){}
+
+    @Get(':nome')
+    public busca(@Param('nome') nome: string){
+        const usuario = this.usuarioService.buscaNome(nome);
+        return usuario;
+    }
 
     @Post()
-    public store(@Body() usuario){
+    public store(@Body() usuario: Usuario): Usuario{
         const usuarioCriado = this.usuarioService.store(usuario)
         
         return usuarioCriado;
--- nestjs/aula2/src/usuario/usuario.service.ts
+++ nestjs/aula2/src/usuario/usuario.service.ts
@@ -1,9 +1,23 @@
-export class UsuarioService{
-    private usuarios = [];
+import { Injectable } from "@nestjs/common";
+import { Usuario } from "./usuario.entity";
 
-    public store(usuario){
+@Injectable()
+export class UsuarioService{
+    private usuarios: Array<Usuario> = [{
+            id:1,
+            nome: 'lucas',
+            email: 'lucas',
+            senha: 'lucas',
+        }
+    ];
+
+    public store(usuario: Usuario): Usuario{
         this.usuarios.push(usuario);
         
         return usuario;
+    }
+
+    public buscaNome(nome: string): Usuario{
+        return this.usuarios.find(usuario => usuario.nome == nome)
     }
 }
